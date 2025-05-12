66566dd835d32b47fa103bfd193d57a61ca44852
Jhon Paternina <jhonpaternina2002@gmail.com>
2025-05-12 17:37:34 -0500

    Organizando Estructura

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 //import Login from './login.jsx'
-import Login2 from './login2.jsx'
-import MisCursos from './Miscursos.jsx'
+import Login2 from './loginPage.jsx'
 import CoursesPage from './CoursesPage.jsx';
 
 createRoot(document.getElementById('root')).render(
